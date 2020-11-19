66be46a17e969f013b32857c7f6c74b2e5702c65
sharon1160 <schullunquia@unsa.edu.pe>
2020-11-19 12:04:02 -0500

    Adding datas

---
+++ iknow/lib/screens/home/components/docs.dart
@@ -0,0 +1,31 @@
+class Doc {
+  final String name;
+  final String doc;
+  final String fecha;
+  final int code;
+
+  const Doc({
+    this.name,
+    this.doc,
+    this.fecha,
+    this.code
+  });
+}
+
+final countries = [
+  new Doc(name:'Jaime Francisco',doc:'Doc. Nro 0001',fecha:'11-01-2020',code: 9498700),
+  new Doc(name:'Andrea Caceres',doc:'Doc. Nro 0002',fecha:'11-01-2020',code: 7153784),
+  new Doc(name:'Ruth Silvana Tejada',doc:'Doc. Nro 0003',fecha:'11-01-2020',code: 10558524),
+  new Doc(name:'Ariana Espezua',doc:'Doc. Nro 0004',fecha:'11-01-2020',code: 5717014),
+  new Doc(name:'Ana Solorzano',doc:'Doc. Nro 0005',fecha:'11-01-2020',code: 60665551),
+  new Doc(name:'Pedro Castillo',doc:'Doc. Nro 0006',fecha:'11-01-2020',code: 37623),
+  new Doc(name:'Cecilia Vilca',doc:'Doc. Nro 0007',fecha:'11-01-2020',code: 5223256),
+  new Doc(name:'Carlos Atencio',doc:'Doc. Nro 0008',fecha:'11-01-2020',code: 46438422),
+  new Doc(name:'Fernanda Villareal',doc:'Doc. Nro 0009',fecha:'11-01-2020',code: 9894888),
+  new Doc(name:'Camilo Valverde',doc:'Doc. Nro 0010',fecha:'11-01-2020',code: 42692393),
+  new Doc(name:'Carmen Castillo',doc:'Doc. Nro 0011',fecha:'11-01-2020',code: 37623),
+  new Doc(name:'Katy Vilca',doc:'Doc. Nro 0012',fecha:'11-01-2020',code: 5323256),
+  new Doc(name:'CarlaAtencio',doc:'Doc. Nro 0013',fecha:'11-01-2020',code: 46433422),
+  new Doc(name:'Fabiola Villareal',doc:'Doc. Nro 0014',fecha:'11-01-2020',code: 9892888),
+  new Doc(name:'Caroline Valverde',doc:'Doc. Nro 0015',fecha:'11-01-2020',code: 42632393),
+];
