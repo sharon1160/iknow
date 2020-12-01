0322f48987deca65cd300b574c9bbe825d79e6e3
sharon1160 <schullunquia@unsa.edu.pe>
2020-11-30 19:23:28 -0500

    Updating home page

--- iknow/lib/screens/home/home_screen.dart
+++ iknow/lib/screens/home/home_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:iknow/screens/home/components/docs.dart';
+// import 'package:flutter/semantics.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:iknow/screens/home/widgets/bottom_nav_bar.dart';
+import 'package:iknow/constants.dart';
+import 'package:iknow/screens/plantillas/plantillas_screen.dart';
+import 'package:iknow/screens/home/widgets/category_card.dart';
+// import 'package:iknow/screens/home/components/docs.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -14,30 +20,123 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Lista de Documentos',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
+        //primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-              title: Text('Lista de Documentos')
-          ),
-          body: new ListView(
-              children: countries.map(_buildItem).toList(),
-          )
-      )
+      home: HomeScreen2(),
     );
   }
 }
 
-Widget _buildItem(Doc doc) {
-  return new ListTile(
-      title: new Text(doc.name),
-      subtitle: new Text('Documento: ${doc.doc}'),
-      trailing: new Text('Fecha: ${doc.fecha}'),
-      leading: new Icon(Icons.person),
-      onTap: (){
-        print(doc.name);
-      },
-  );
+class HomeScreen2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    var size = MediaQuery.of(context)
+        .size;
+    return Scaffold(
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color(0xFF81f2dd),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/Img/image2.png"),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                        color: Color(0xff24dcbb),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Welcome to iKnow !",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(fontWeight: FontWeight.w300),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 30),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 1),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(7),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        icon: SvgPicture.asset("assets/icons/search.svg"),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: .85,
+                      crossAxisSpacing: 15,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        CategoryCard(
+                          title: "Documentos Recientes",
+                          svgSrc: "assets/Img/icons8-pasado-48.svg",
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: "Plantillas",
+                          svgSrc: "assets/Img/icons8-formularios-de-google-48.svg",
+                          press: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context){
+                                return PlantillasScreen();
+                              }),
+                            );
+                          },
+                        ),
+                        CategoryCard(
+                          title: "Mis Documentos",
+                          svgSrc: "assets/Img/icons8-guardar-48.svg",
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
 }
+
+
+
+
+
+
