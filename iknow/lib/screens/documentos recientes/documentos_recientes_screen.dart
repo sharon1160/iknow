5a17e100a82bd5a7fca372baa839ca19581fc6de
sharon1160 <schullunquia@unsa.edu.pe>
2020-12-02 19:53:20 -0500

    Adding Collapsing Toolbar

--- iknow/lib/screens/documentos recientes/documentos_recientes_screen.dart
+++ iknow/lib/screens/documentos recientes/documentos_recientes_screen.dart
@@ -47,108 +47,119 @@ class _DocumentosRecientesScreenState extends State<DocumentosRecientesScreen> {
     // MediaQuery to get Device Width
     double width = MediaQuery.of(context).size.width * 0.44;
     return Scaffold(
-      appBar: AppBar(
-        // App Bar
-        title: Text(
-          "Documentos Recientes",
-          style: Theme.of(context)
-              .textTheme
-              .headline6
-              .copyWith(fontWeight: FontWeight.w300),
-        ),
-        elevation: 0,
-        backgroundColor: Color(0xff24dcbb),
-      ),
-      // Main List View With Builder
-      body: ListView.builder(
-        itemCount: imgList.length,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () {
-              // This Will Call When User Click On ListView Item
-              //showDialogFunc(context, imgList[index], titleList[index], descList[index]);
-            },
-            // Card Which Holds Layout Of ListView Item
-            child: Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: 80,
-                    height: 80,
-                    child: Image.asset(imgList[index]),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              backgroundColor: Color(0xff24dcbb),
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(
+                    "Documentos Recientes",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(fontWeight: FontWeight.w300),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          width: width,
-                          child: Text(
-                            titleList[index], // tituloo
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.black,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          width: width,
-                          child: Text(
-                            descList[index], // descripcion
-                            maxLines: 3,
-                            style: TextStyle(
-                                fontSize: 10, color: Colors.grey[500]),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Container(
-                          width: width,
-                          child: Text(
-                            fechList[index], // fecha
-                            maxLines: 3,
-                            style: TextStyle(
-                                fontSize: 10, color: Colors.grey[500]),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context){
-                          return HomeScreen2();
-                        }),
-                      );
-                    },
-                    child: Align(
-                      alignment: Alignment.topRight,
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 90,//52
-                        width: 90,
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                          shape: BoxShape.circle,
-                        ),
-                        child: SvgPicture.asset("assets/icons/descargar.svg"),
-                      ),
-                    ),
-                  )
-                ],
+                  background: SvgPicture.asset("assets/icons/descargar.svg"),
               ),
             ),
-          );
+          ];
         },
+        // Main List View With Builder
+        body: ListView.builder(
+          itemCount: imgList.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                // This Will Call When User Click On ListView Item
+                //showDialogFunc(context, imgList[index], titleList[index], descList[index]);
+              },
+              // Card Which Holds Layout Of ListView Item
+              child: Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 80,
+                      height: 80,
+                      child: Image.asset(imgList[index]),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            width: width,
+                            child: Text(
+                              titleList[index], // tituloo
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            width: width,
+                            child: Text(
+                              descList[index], // descripcion
+                              maxLines: 3,
+                              style: TextStyle(
+                                  fontSize: 10, color: Colors.grey[500]),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Container(
+                            width: width,
+                            child: Text(
+                              fechList[index], // fecha
+                              maxLines: 3,
+                              style: TextStyle(
+                                  fontSize: 10, color: Colors.grey[500]),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context){
+                            return HomeScreen2();
+                          }),
+                        );
+                      },
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 90,//52
+                          width: 90,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            shape: BoxShape.circle,
+                          ),
+                          child: SvgPicture.asset("assets/icons/descargar.svg"),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
