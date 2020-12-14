0e089ca3b58a1de1b62d65fa178d0f0a3eac0a90
sharon1160 <schullunquia@unsa.edu.pe>
2020-12-14 08:03:32 -0500

    Commenting an unused import

--- iknow/lib/controllers/authentications.dart
+++ iknow/lib/controllers/authentications.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 final gooleSignIn = GoogleSignIn();
